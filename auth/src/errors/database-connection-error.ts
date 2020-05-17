2a1850c2668978edf83fbe5d26cf166ab719db90
Adnan Reza <adnan.reza@outlook.com>
2020-05-17 14:15:48 -0700

    Refactored logic from middleware to custom errors

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,8 +1,13 @@
 export class DatabaseConnectionError extends Error {
+  statusCode = 500;
   reason = 'Error connecting to database';
   constructor() {
     super();
     // Only because we are extending built-in class
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+
+  serializeErrors() {
+    return [{ message: this.reason }];
   }
 }
