84bf5a64ed2b6763b754585b55cd31718605271c
Adnan Reza <adnan.reza@outlook.com>
2020-05-17 12:31:25 -0700

    Added error handling middleware

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -16,10 +16,12 @@ router.post(
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      return res.status(400).send(errors.array());
+      throw new Error('Invalid email or password');
     }
 
     console.log('Creating a user...');
+    throw new Error('Error connecting to database');
+
     res.send({});
 
     const { email, password } = req.body;
