b6a86a640be2552964b839830f9ceb1ca421b893
Adnan Reza <adnan.reza@outlook.com>
2020-05-20 13:01:47 -0700

    Generated, signed and stored JWT for signin route handler

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+import { User } from '../models/user';
 import { validateRequest } from '../middlewares/validate-request';
+import { Password } from '../utils/password';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -11,7 +15,35 @@ router.post(
     body('password').trim().notEmpty().withMessage('You must enter a password'),
   ],
   validateRequest,
-  (req: Request, res: Response) => {}
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError('Invalid credentials');
+    }
+    const pwMatch = await Password.compare(existingUser.password, password);
+
+    if (!pwMatch) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    /**Generate JWT */
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    /**Store JWT on session object */
+    req.session = {
+      jwt: userJwt,
+    };
+
+    res.status(200).send(existingUser);
+  }
 );
 
 export { router as signinRouter };
