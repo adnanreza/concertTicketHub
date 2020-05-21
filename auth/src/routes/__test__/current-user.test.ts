babb4b0482d1de5bbfc45bf66f416afa4da06547
Adnan Reza <adnan.reza@outlook.com>
2020-05-21 13:39:18 -0700

    Refactored cookie retrieval process to global getAuthCookie method in setup.ts

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -2,14 +2,7 @@ import request from 'supertest';
 import { app } from '../../app';
 
 it('responds with details about current user', async () => {
-  const signupResponse = await request(app)
-    .post('/api/users/signup')
-    .send({
-      email: 'test@test.com',
-      password: 'password',
-    })
-    .expect(201);
-  const cookie = signupResponse.get('Set-Cookie');
+  const cookie = await global.getAuthCookie();
 
   const response = await request(app)
     .get('/api/users/currentuser')
--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,6 +1,17 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import request from 'supertest';
 import { app } from '../app';
+
+/**Augment Global */
+declare global {
+  namespace NodeJS {
+    interface Global {
+      /**Promise resolved as array of strings */
+      getAuthCookie(): Promise<string[]>;
+    }
+  }
+}
 
 let mongo: any;
 
@@ -31,3 +42,21 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+/**Added GetAuthCookie method to NodeJS global scope to return cookie if signed in */
+global.getAuthCookie = async () => {
+  const email = 'test@test.com';
+  const password = 'password';
+
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email,
+      password,
+    })
+    .expect(201);
+
+  const cookie = response.get('Set-Cookie');
+
+  return cookie;
+};
