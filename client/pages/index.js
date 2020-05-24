19a80c12e244369d7ebc89c1868d426aceace486
Adnan Reza <adnan.reza@outlook.com>
2020-05-24 12:52:13 -0700

    Implemented cross namespace comms from client to ingress-srv

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,32 @@
-export default () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
   return <h1>Landing Page!!!</h1>;
 };
+
+LandingPage.getInitialProps = async () => {
+  // const response = await axios.get(
+  //   'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser'
+  // );
+  if (typeof window === 'undefined') {
+    // we are on the server
+    const response = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: {
+          Host: 'ticketing.dev',
+        },
+      }
+    );
+
+    return response.data;
+  } else {
+    const response = await axios.get('/api/users/currentuser');
+    return response.data;
+  }
+  console.log('I was executed on the server');
+  return {};
+};
+
+export default LandingPage;
