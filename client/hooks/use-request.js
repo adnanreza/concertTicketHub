091c8c81905dd1272cf9349ec4b4cfe345fb0c29
Adnan Reza <adnan.reza@outlook.com>
2020-05-23 19:50:21 -0700

    Added an onSuccess callback to use-request custom hook

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -1,13 +1,16 @@
 import axios from 'axios';
 import { useState } from 'react';
 
-export default ({ url, method, body }) => {
+export default ({ url, method, body, onSuccess }) => {
   const [errors, setErrors] = useState(null);
 
   const doRequest = async () => {
     try {
       setErrors(null); // clear out old errors on new req attempt
       const response = await axios[method](url, body);
+      if (onSuccess) {
+        onSuccess(response.data);
+      }
       return response.data;
     } catch (err) {
       setErrors(
