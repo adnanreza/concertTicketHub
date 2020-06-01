e2623b57cd6598af05e122c823ff3d638114ce51
Adnan Reza <adnan.reza@outlook.com>
2020-06-01 09:48:41 -0700

    Moved common code to common libary in npm and modified auth service to reflect it

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -2,9 +2,8 @@ import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
 import { User } from '../models/user';
-import { validateRequest } from '../middlewares/validate-request';
+import { validateRequest, BadRequestError } from '@arconcerttickets/common';
 import { Password } from '../utils/password';
-import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
