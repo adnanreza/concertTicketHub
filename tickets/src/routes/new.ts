03c29877dc4d2280e55f299def38993644eaa804
Adnan Reza <adnan.reza@outlook.com>
2020-06-01 13:09:17 -0700

    Wrote tests to validate price and ticket and made them pass

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,13 +1,25 @@
 import express, { Request, Response, request } from 'express';
-import { requireAuth } from '@arconcerttickets/common';
+import { body } from 'express-validator';
+import { requireAuth, validateRequest } from '@arconcerttickets/common';
 
 const router = express.Router();
 
 /**
  * Route handler to create new ticket
  */
-router.post('/api/tickets', requireAuth, (req: Request, res: Response) => {
-  res.sendStatus(200);
-});
+router.post(
+  '/api/tickets',
+  requireAuth,
+  [
+    body('title').not().isEmpty().withMessage('Title is required'),
+    body('price')
+      .isFloat({ gt: 0 })
+      .withMessage('Price must be greater than 0'),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {
+    res.sendStatus(200);
+  }
+);
 
 export { router as createTicketRouter };
