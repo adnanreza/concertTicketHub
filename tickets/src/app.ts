768aa1f1254f628b3f6732fcaf7962e671c66a70
Adnan Reza <adnan.reza@outlook.com>
2020-06-02 13:59:52 -0700

    Wrote tests for updateTicket and implemented route handler to make tests pass

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -11,6 +11,7 @@ import {
 import { createTicketRouter } from './routes/new';
 import { showTicketRouter } from './routes/show';
 import { indexTicketRouter } from './routes/index';
+import { updateTicketRouter } from './routes/update';
 
 /**Configure Express App */
 
@@ -28,6 +29,7 @@ app.use(currentUser);
 app.use(createTicketRouter);
 app.use(showTicketRouter);
 app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all('*', async () => {
   throw new NotFoundError();
---
+++ tickets/src/routes/__test__/update.test.ts
@@ -0,0 +1,49 @@
+import request from 'supertest';
+import { app } from '../../app';
+import mongoose from 'mongoose';
+
+it('returns a 404 if the provided id does not exist', async () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
+  await request(app)
+    .put(`/api/tickets/${id}`)
+    .set('Cookie', global.getAuthCookie())
+    .send({
+      title: 'testTitle',
+      price: 20,
+    })
+    .expect(404);
+});
+
+it('returns a 401 if the user is not authenticated', async () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
+  await request(app)
+    .put(`/api/tickets/${id}`)
+    .send({
+      title: 'testTitle',
+      price: 20,
+    })
+    .expect(401);
+});
+
+it('returns a 401 if the user does not own the ticket', async () => {
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.getAuthCookie())
+    .send({
+      title: 'newlyCreatedTestTicket',
+      price: 23,
+    });
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', global.getAuthCookie())
+    .send({
+      title: 'updateName',
+      price: 25,
+    })
+    .expect(401);
+});
+
+it('returns a 400 if the user provides an invalid title or price', async () => {});
+
+it('updates the ticket if provided valid inputs', async () => {});
